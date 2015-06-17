9deb0bf8dd74866b27ab746ec694d22a71fe89a1
Ciprian Radu <cypryradu@gmail.com>
2015-06-18 00:16:27 +0300

    Add docblocks to Field.php and Table.php + gitignoring the docs/ folder

--- src/ValueObject/Field.php
+++ src/ValueObject/Field.php
@@ -1,22 +1,51 @@
 <?php
 namespace CypryRadu\AdvancedFilter\ValueObject;
 
+/**
+* Encapsulates the data for a single fields
+*
+* @author Ciprian Radu <cypryradu@gmail.com>
+*/
 class Field 
 {
+    /**
+     * @var string
+     */
     private $fieldKey;
+
+    /**
+     * @var array
+     */
     private $metaData;
     
-    public function __construct($fieldKey, $metaData)
+    /**
+     * Constructor
+     *
+     * @param string $fieldKey
+     * @param array $metaData
+     *
+     */
+    public function __construct($fieldKey, array $metaData)
     {
         $this->fieldKey = $fieldKey;
         $this->metaData = $metaData;
     }
     
+    /**
+     * Gets the field's key
+     *
+     * @return string
+     */
     public function getKey()
     {
         return $this->fieldKey;
     }
     
+    /**
+     * Gets the the field's name in the database
+     *
+     * @return string
+     */
     public function getDbField()
     {
         return (!empty($this->metaData['db_field']) 
@@ -25,6 +54,11 @@ class Field
         );
     }
     
+    /**
+     * Gets the tables to be used by this field
+     *
+     * @return array
+     */
     public function getUseTables()
     {
         if (!isset($this->metaData['use_tables']) || !is_array($this->metaData['use_tables'])) {
@@ -34,15 +68,23 @@ class Field
         return $this->metaData['use_tables'];
     }
     
+    /**
+     * Gets the field's table alias
+     *
+     * @return string
+     */
     public function getTableAlias()
     {
         return (!empty($this->metaData['table_alias']) ? $this->metaData['table_alias'] . '.' : '');
     }
     
+    /**
+     * Gets the field's table alias
+     *
+     * @return string
+     */
     public function getFieldName()
     {
         return $this->getTableAlias() . '`' . $this->getDbField() . '`';
     }
-    
-    
 }
--- src/ValueObject/Table.php
+++ src/ValueObject/Table.php
@@ -1,18 +1,56 @@
 <?php
 namespace CypryRadu\AdvancedFilter\ValueObject;
 
+/**
+* Encapsulates the data for a single table
+*
+* This class is immutable
+*
+* @author Ciprian Radu <cypryradu@gmail.com>
+*/
 class Table 
 {
+    /**
+     * @var string
+     */
     private $tableKey;
+
+    /**
+     * @var string
+     */
     private $tableName;
+
+    /**
+     * @var string
+     */
     private $tableAlias;
+
+    /**
+     * @var array
+     */
     private $metaData;
+
+    /**
+     * @var string
+     */
     private $joinType;
+
+    /**
+     * @var string
+     */
     private $joinOn;
     
+    /**
+     * Constructor
+     *
+     * @param string $tableKey
+     * @param array $metaData
+     *
+     */
     public function __construct($tableKey, $metaData)
     {
         list($tableName, $tableAlias) = array_reverse(explode('.', $tableKey));
+
         $this->tableKey = $tableKey;
         $this->tableName = $tableName;
         $this->tableAlias = $tableAlias;
@@ -21,29 +59,53 @@ class Table
         $this->joinOn = array_shift($metaData);
     }
     
+    /**
+     * Gets the tables's key
+     *
+     * @return string
+     */
     public function getKey()
     {
         return $this->tableKey;
     }
     
+    /**
+     * Gets the tables's name
+     *
+     * @return string
+     */
     public function getName()
     {
         return $this->tableName;
     }
     
+    /**
+     * Gets the tables's alias
+     *
+     * @return string
+     */
     public function getAlias()
     {
         return $this->tableAlias;
     }
     
+    /**
+     * Gets the tables's join type with the other tables
+     *
+     * @return string
+     */
     public function getJoinType()
     {
         return $this->joinType;
     }
     
+    /**
+     * Gets the tables's join ON condition
+     *
+     * @return string
+     */
     public function getJoinOn()
     {
         return $this->joinOn;
     }
-    
 }
